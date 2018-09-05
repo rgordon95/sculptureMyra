fe42ec5a5f569d88967800b6bf6481919a613b74
Richard Gordon <richgordon95@gmail.com>
2018-09-05 16:15:04 -0400

    major changes (php implementation and boostrap 4.0 fixes)

---
+++ php/contact_form.php
@@ -0,0 +1,14 @@
+<?php
+    		$name=$_POST['name'];
+    		$email=$_POST['email'];
+    		$phone=$_POST['phone'];
+    		$msg=$_POST['msg'];
+    		$to='sculpturemyra@optonline.net'; // Receiver Email ID, Replace with your email ID
+    		$subject='New Email from sculpturemyra.com visitor';
+    		$message="Name :".$name."\n"."Phone :".$phone."\n"."Wrote the following :"."\n\n".$msg;
+    		$headers="From: sculpturemyra@optonline.net";
+    		if(mail($to, $subject, $message, $headers)){
+                header("Location: ../index.html");
+    		}
+    }
+?>
---
+++ php/sculpture_inquiry.php
@@ -0,0 +1,16 @@
+<?php
+    		$name=$_POST['name'];
+    		$email=$_POST['email'];
+    		$phone=$_POST['phone'];
+        $piece=$_POST['piece'];
+        $price=$_POST['price'];
+    		$msg=$_POST['msg'];
+    		$to='sculpturemyra@optonline.net'; // Receiver Email ID, Replace with your email ID
+    		$subject='New Email from sculpturemyra.com visitor';
+    		$message="Name :".$name."\n"."Phone :".$phone."\n"."requested the following:"."\n\n".$piece "\n\n" $price "\n\n" $msg;
+    		$headers="From: sculpturemyra@optonline.net";
+    		if(mail($to, $subject, $message, $headers)){
+                header("Location: ../index.html");
+    		}
+    }
+?>
