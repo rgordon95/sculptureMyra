0f39d27112663a7cac3a89eca7d1333863da30f2
Richard Gordon <richgordon95@gmail.com>
2018-09-26 01:29:56 -0400

    update php

--- php/contact_form.php
+++ php/contact_form.php
@@ -6,7 +6,7 @@
     		$to='sculpturemyra@optonline.net'; // Receiver Email ID, Replace with your email ID
     		$subject='New Email from sculpturemyra.com visitor';
     		$message="Name :".$name."\n"."Phone :".$phone."\n"."Wrote the following :"."\n\n".$msg;
-    		$headers="From: sculpturemyra@optonline.net";
+    		$headers="From: richalbreb@gmail.com.";
     		if(mail($to, $subject, $message, $headers)){
                 header("Location: ../index.html");
     		}
--- php/sculpture_inquiry.php
+++ php/sculpture_inquiry.php
@@ -8,7 +8,7 @@
     		$to='sculpturemyra@optonline.net'; // Receiver Email ID, Replace with your email ID
     		$subject='New Email from sculpturemyra.com visitor';
     		$message="Name :".$name."\n"."Phone :".$phone."\n"."requested the following:"."\n\n".$piece."\n\n".$price."\n\n".$msg;
-    		$headers="From: sculpturemyra@optonline.net";
+    		$headers="From: richalbreb@gmail.com.";
     		if(mail($to, $subject, $message, $headers)){
                 header("Location: ../index.html");
     		}
