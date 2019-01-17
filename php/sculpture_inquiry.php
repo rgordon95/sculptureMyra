f00fb14860cbddab30eb8a084f93852aeb4f7ff5
Richard Gordon <richgordon95@gmail.com>
2019-01-16 20:14:05 -0500

    add captcha script to all pages

--- php/sculpture_inquiry.php
+++ php/sculpture_inquiry.php
@@ -6,7 +6,7 @@
         $price=$_POST['price'];
     		$msg=$_POST['msg'];
     		$to='sculpturemyra@optonline.net'; // Receiver Email ID, Replace with your email ID
-    		$subject='New Email from sculpturemyra.com visitor';
+    		$subject='New Email from sculpturemyra.com visitor Sculpture Form';
     		$message="Name :".$name."\n"."Phone :".$phone."\n"."requested the following:"."\n\n".$piece."\n\n".$price."\n\n".$msg;
     		$headers="From: richalbreb@gmail.com.";
     		if(mail($to, $subject, $message, $headers)){
