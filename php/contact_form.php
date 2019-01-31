fa143d8e4128cf73eab680c220a539c2d07880bd
Richard Gordon <richgordon95@gmail.com>
2019-01-31 17:29:23 -0500

    captcha fixes

--- php/contact_form.php
+++ php/contact_form.php
@@ -4,7 +4,7 @@
     		$phone=$_POST['phone'];
     		$msg=$_POST['msg'];
     		$to='sculpturemyra@optonline.net'; // Receiver Email ID, Replace with your email ID
-    		$subject='New Email from sculpturemyra.com visitor';
+    		$subject='New Email from sculpturemyra.com visitor (contact form)';
     		$message="Name :".$name."\n"."Phone :".$phone."\n"."Wrote the following :"."\n\n".$msg;
     		$headers="From: richalbreb@gmail.com.";
     		if(mail($to, $subject, $message, $headers)){
